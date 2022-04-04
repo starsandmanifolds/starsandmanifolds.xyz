8bf240d04409e3c722984f16fb327a1ce0b8905f
Anand Dyavanapalli <adyavanapalli@gmail.com>
2022-04-03 22:42:53 -0400

    chore: Remove unneeded outputs.

--- terraform/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "ssh_connect_command" {
-  description = "The command to use to connect to the virtual machine over SSH."
-  value       = "ssh ${var.username}@${google_compute_instance.instance.network_interface.0.access_config.0.nat_ip}"
-}
